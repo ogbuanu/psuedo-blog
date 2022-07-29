9933b9371f28affc302e9018833d2fd30d88e04c
Ezeh chiadikaobi <ezehdavid67@gmail.com>
2022-07-29 10:30:29 +0100

    majour changes made

--- routes/editpost.js
+++
@@ -1,8 +0,0 @@
-const editPost = require("express").Router()
-const {editPostController} = require("../controllers/editPostController")
-editPost.get("/editPost", editPostController)
-const {editPostController, editPostControllerPost} = require("../controllers/editPostController")
-editPost.get("/editPost/:postId", editPostController)
-editPost.post("/editpost/:postId", editPostControllerPost)
-
-exports.editPostRoute = editPost 
